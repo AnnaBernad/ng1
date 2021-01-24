bf29b1d44375670bb71ddf44c7f181089cf1d155
AnnaBernad <73069123+AnnaBernad@users.noreply.github.com>
2021-01-25 01:01:34 +0200

    .

---
+++ src/db/data.ts
@@ -0,0 +1,7 @@
+ export let cars = [
+  {model: 'opel', color: 'red'},
+  {model: 'tesla', color: 'black'},
+  {model: 'nissan', color: 'white'},
+  {model: 'shkoda', color: 'blue'},
+  {model: 'mazda', color: 'green'},
+];
